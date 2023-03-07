441f07be3f7d191a2a2214299c5f01634cfda99f
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-03-07 19:26:36 +0600

    added

--- app.js
+++ app.js
@@ -17,3 +17,36 @@ let count = 0;
     document.getElementById("stop").addEventListener('click', function(){
         clearInterval(interValId)
     });
+
+// this is header banner text info
+
+const bannerTitles = ["Welcome To My Website", "MD.Ashaduzzaman Sojib", "Professional Web Developer"];
+
+let bannerIndex = 0;
+const bannerTitle = document.getElementById("banner-title");
+
+setInterval(() => {
+  bannerTitle.innerText = bannerTitles[bannerIndex];
+  bannerIndex = (bannerIndex + 1) % bannerTitles.length;
+}, 800);
+
+// const texts = ["I'm Professional Web Developer", "Web Application Maker", "Top Rated SEO Expert!"];
+// let textIndex = 0;
+
+// let displayText = "";
+// let charIndex = 0;
+
+// const displayElement = document.getElementById("banner-title");
+
+// setInterval(() => {
+//   const text = texts[textIndex];
+//   displayText += text.charAt(charIndex);
+//   charIndex++;
+
+//   if (charIndex > text.length) {
+//     charIndex = 0;
+//     textIndex = (textIndex + 1) % texts.length;
+//   }
+
+//   displayElement.innerText = displayText;
+// }, 100);
