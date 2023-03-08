8e31d13416b5c480a458c2b60f16fcd0be0636de
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-03-08 22:20:45 +0600

    added modal function

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// dropdown menu default style
+const dropdownList = document.querySelectorAll('.nav-link');
+for(const dropdown of dropdownList){
+    dropdown.classList.add('block', 'text-2xl', 'font-semibold', 'p-2')
+}
+// sidebar title desing
+const sidebarTitle = document.querySelectorAll('.sidebar-title');
+for(const title of sidebarTitle){
+    title.classList.add('py-2', 'bg-slate-500', 'text-center', 'font-semibold');
+}
 let count = 0;
     let interValId;
     // start inter val
@@ -30,6 +40,13 @@ setInterval(() => {
   bannerIndex = (bannerIndex + 1) % bannerTitles.length;
 }, 800);
 
+const loadModal = () =>{
+    const container = document.getElementById("my-modal");
+    container.innerHTML = `
+    
+    `;
+}
+// loadModal()
 // const texts = ["I'm Professional Web Developer", "Web Application Maker", "Top Rated SEO Expert!"];
 // let textIndex = 0;
 
