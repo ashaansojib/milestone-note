6ada80ec46a0c4393c676de69130f06ed74214c7
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-03-08 15:27:42 +0600

    fixed total

--- error-projects/ranga-store-vanga/js/app.js
+++ error-projects/ranga-store-vanga/js/app.js
@@ -13,13 +13,13 @@ loadProducts('https://fakestoreapi.com/products');
 
 // show all product in UI
 const showProducts = (products) => {
-   console.log(products)
    setInnerText('total_products', products.length);
 
    document.getElementById("all-products").innerHTML = "";
 
    const allProducts = products.slice(0, 10).map((pd) => pd);
    for (const product of allProducts) {
+      console.log(product)
       const image = product.image;
 
       const div = document.createElement('div');
@@ -45,7 +45,7 @@ let count = 0;
 
 const addToCart = (id, price) => {
    count = count + 1;
-   updatePrice('price', value);
+   updatePrice('price', price);
 
    updateTaxAndCharge();
    document.getElementById('total-Products').innerText = count;
@@ -68,21 +68,21 @@ const showProductDetailsInModal = (product_details) => {
 
 const getInputValue = (id) => {
    const element = document.getElementById(id).innerText;
-   const converted = parseInt(element);
+   const converted = parseFloat(element);
    return converted;
 };
 
 // main price update function
 const updatePrice = (id, value) => {
    const convertedOldPrice = getInputValue(id);
-   const convertPrice = parseInt(value);
+   const convertPrice = parseFloat(value);
    const total = convertedOldPrice + convertPrice;
-   document.getElementById(id).innerText = Math.round(total);
+   document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-   document.getElementById(id).innerText = Math.round(value);
+   document.getElementById(id).innerText = value;
 };
 
 // update delivery charge and total Tax
@@ -108,7 +108,7 @@ const updateTotal = () => {
       getInputValue('price') +
       getInputValue('delivery-charge') +
       getInputValue('total-tax');
-   document.getElementById('total').innerText = grandTotal;
+   document.getElementById('total').innerText = grandTotal.toFixed(2);
 };
 
 // search by category
